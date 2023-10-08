7d4f89045bef3ea74c882da349bbc27dc3d7ca0c
soukaynabensalah <146835722+soukaynabensalah@users.noreply.github.com>
2023-10-08 22:18:44 +0100

    Create Category.py

---
+++ Category.py
@@ -0,0 +1,11 @@
+age=(input("veuillez enter l'âge de l'enfant"))
+if age>=6 and age=<7 :
+  print ("poussin")
+elif age>=8 and age<=9 :
+  print("pupile")
+elif age>=10 and age<=11 :
+  print("minime")
+elif age<=12 :
+  print("cadet")
+else 
+  print("désolé ! je ne sais pas la catégorie de cet âge")
